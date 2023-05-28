b04bd60d1afa89cbba0177fe402166c36746a8d3
haeun <che9663@naver.com>
2023-05-28 22:37:36 +0900

    hello Thymeleaf

---
+++ src/main/java/com/exam/examproject/controller/ExamController.java
@@ -0,0 +1,12 @@
+package com.exam.examproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExamController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "/hello.html";
+    }
+}
